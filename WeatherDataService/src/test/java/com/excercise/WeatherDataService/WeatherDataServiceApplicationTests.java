4fd99e7aa8b5e6cb5e020c2e451cfae233a605ae
Brayan Díaz <brayandiaz258f@gmail.com>
2025-06-08 14:30:21 -0500

    Cafesito con leche, subiendo el proyecto spring inicial.

---
+++ WeatherDataService/src/test/java/com/excercise/WeatherDataService/WeatherDataServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.excercise.WeatherDataService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherDataServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
