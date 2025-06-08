240c3c0a4e962fc0ccb1fb0d06211240cd5ee687
Brayan Díaz <brayandiaz258f@gmail.com>
2025-06-08 18:38:07 -0500

    Create controller para permitir el consumo de la API, por medio del método POST. Utilizar el servicio de manera reactiva, para retornar el DTO que esperamos.

--- WeatherDataService/src/main/java/com/excercise/WeatherDataService/service/WeatherResponseService.java
+++ WeatherDataService/src/main/java/com/excercise/WeatherDataService/service/WeatherResponseService.java
@@ -2,19 +2,17 @@ package com.excercise.WeatherDataService.service;
 
 import com.excercise.WeatherDataService.model.apiDTO.WeatherResponseDTO;
 import com.excercise.WeatherDataService.model.responseDTO.ResponseDTO;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class WeatherResponseService {
 
-    @Autowired
-    GetDataWeatherService dataWeatherService;
-
     public ResponseDTO serviceResponse(WeatherResponseDTO weatherResponseDTO){
+
         Boolean cold = weatherResponseDTO.temperature() < 10;
         Boolean windy = weatherResponseDTO.windSpeed() > 10;
         Boolean humid = weatherResponseDTO.humidity() > 80;
+
         return new ResponseDTO(
                 weatherResponseDTO.cityName(),
                 weatherResponseDTO.temperature(),
@@ -25,6 +23,4 @@ public class WeatherResponseService {
                 weatherResponseDTO.windSpeed()
                 );
     }
-
-
 }
